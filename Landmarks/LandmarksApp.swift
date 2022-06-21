0d444a657a2d55bce1c8de9722184430859fee61
Carlos Corvalan <ccorvalandev@outlook.com>
2022-06-21 09:47:54 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Carlos Corvalan on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
