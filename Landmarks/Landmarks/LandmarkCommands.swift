e3ce7edfc5b470f5419e4ef74268cda3bc1bae31
Carlos Corvalan <ccorvalandev@outlook.com>
2022-07-16 19:51:33 -0400

    Add a Built-in Menu Command

---
+++ Landmarks/Landmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Carlos Corvalan on 7/16/22.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
